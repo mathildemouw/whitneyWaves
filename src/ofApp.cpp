e0796a740278558d589a9cda3ddea4e466f16384
Mathilde <nihaomouw@mac.com>
2019-10-15 16:49:58 -0400

    Add pulsing blue circle and red concentric circles

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -13,13 +13,26 @@ void ofApp::update(){
 //--------------------------------------------------------------
 void ofApp::draw(){
 
+    ofFill();
+    ofSetColor(99, 199, 200);
+    // slowly growing and shrinking teal circle
+    ofDrawCircle(ofGetScreenWidth()/4, ofGetScreenHeight()/2, 900 * sin(ofGetElapsedTimef()/10));
+
     // The Good Place green
     ofSetColor(89, 189, 100);
 
+    // green dots moving in a pattern
     float i;
-    for (i=10; i < 800; i+=  20) {
-        float x = ofMap(sin(ofGetElapsedTimef()* (i * 0.01)), -1, 1, 30, 1000);
+    for (i=10; i < 800; i+= 20) {
+        float x = ofMap(sin(ofGetElapsedTimef()* (i * 0.01)), -1, 1, 30, ofGetScreenWidth()/2);
         ofDrawCircle(x, i, 10);
+    }
+
+    ofSetColor(300, 199, 200);
+    ofNoFill();
+    ofSetLineWidth(3);
+    for (i=10; i < 21; i++){
+        ofDrawCircle(ofGetScreenWidth()/4, ofGetScreenHeight()/2, 50 * i * 0.3);
     }
 }
 
